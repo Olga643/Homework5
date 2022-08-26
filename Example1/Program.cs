099b94e1a654a1879e5fe770ce4da84648fb366a
Olga Rudson <o.rudson@mail.ru>
2022-08-26 20:00:49 +0300

    Дано число обозначающее день недели. Выяснить является номер дня недели выходным

---
+++ Example1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.WriteLine ("Ведите число от 1 до 7");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+if ( number > 5 )
+{ 
+    Console.WriteLine ("Ура! Выходной");
+}    
+else 
+{
+    Console.WriteLine ("Будний день");
+}
