5c05b67771513b22bf9ad89ed5aaca3f73c30a30
Olga Rudson <o.rudson@mail.ru>
2022-08-26 20:12:57 +0300

    Дано число обозначающее день недели. Выяснить является номер дня недели выходным

---
+++ Example4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти расстояние между точками в пространстве 2D
+Console.WriteLine ("Ведите координату x1");
+int x1 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine ("Ведите координату y1");
+int y1 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine ("Ведите координату x2");
+int x2 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine ("Ведите координату y2");
+int y2 = int.Parse(Console.ReadLine() ?? "0");
+
+var xy = ((x2 - x1)*(x2 - x1)) + ((y2 - y1)*(y2 - y1));
+var distance = Math.Sqrt (xy);
+Console.WriteLine ($"Расстояние между точками равно - {distance}"); 
